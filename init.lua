969b486df14684038948ac42a5267f788d3c5a0d
e7 <jackzxty@126.com>
2024-11-05 20:36:14 +0800

    feat: init project

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- 选项配置
+require('options')
+-- 按键配置
+require('keymaps')
+-- 插件管理（lazy.nvim）
+require('plugins')
---
+++ lua/plugins.lua
@@ -0,0 +1,28 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    -- file explorer
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+})
